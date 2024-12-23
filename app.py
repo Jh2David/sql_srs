0fef133b7b1ee9ec1d473ef1e028c020731ce4c7
Jehan DAVID <jehan.david@pm.me>
2024-12-23 11:54:44 +0100

    comparaison with real answers

--- app.py
+++ app.py
@@ -6,9 +6,6 @@ import streamlit as st
 
 con = duckdb.connect(database="data/exercises_sql_tables.duckdb", read_only=False)
 
-
-#solution_df = duckdb.sql(ANSWER_STR).df()
-
 with st.sidebar:
     theme = st.selectbox(
         "What would you like to review?",
@@ -21,25 +18,30 @@ with st.sidebar:
     exercise = con.execute(f"SELECT * FROM memory_state WHERE theme = '{theme}'").df()
     st.write(exercise)
 
+    exercise_name = exercise.loc[0, "exercise_name"]
+    with open(f"answers/{exercise_name}.sql", "r", encoding="utf-8") as f:
+        answer = f.read()
+
+    solution_df = con.execute(answer).df()
+
 st.header("enter your code:")
 query = st.text_area(label="votre code SQL ici", key="user_input")
 if query:
-   result = con.execute(query).df()
-   st.dataframe(result)
-#
-#     try:
-#         result = result[solution_df.columns]
-#     except KeyError as e:
-#         st.write("Some columns are missing")
-#
-#     n_lines_differences = result.shape[0] - solution_df.shape[0]
-#     if n_lines_differences != 0:
-#         st.write(
-#             f"result has a {n_lines_differences} lines difference with the solution_df"
-#         )
-#
-#     st.dataframe(result.compare(solution_df))
-#
+    result = con.execute(query).df()
+    st.dataframe(result)
+    try:
+        result = result[solution_df.columns]
+    except KeyError as e:
+        st.write("Some columns are missing")
+
+    n_lines_differences = result.shape[0] - solution_df.shape[0]
+    if n_lines_differences != 0:
+        st.write(
+            f"result has a {n_lines_differences} lines difference with the solution_df"
+        )
+
+    st.dataframe(result.compare(solution_df))
+
 tab2, tab3 = st.tabs(["Tables", "Solution"])
 
 with tab2:
@@ -51,8 +53,4 @@ with tab2:
         st.dataframe(df_table)
 
 with tab3:
-    exercise_name = exercise.loc[0, "exercise_name"]
-    with open("answers/beverages_and_food.sql", "r") as f:
-        answer = f.read()
     st.code(answer)
-
--- init_db.py
+++ init_db.py
@@ -1,6 +1,7 @@
 import io
-import pandas as pd
+
 import duckdb
+import pandas as pd
 
 con = duckdb.connect(database="data/exercises_sql_tables.duckdb", read_only=False)
 
@@ -13,10 +14,9 @@ data = {
     "exercise_name": ["beverages_and_food", "simple_window"],
     "tables": [["beverages", "food_items"], "simple_window"],
     "last_reviewed": ["1970-01-01", "1970-01-01"],
-    "answer": ["SELECT * FROM beverages CROSS JOIN food_items", ""]
 }
 memory_state_df = pd.DataFrame(data)
-#con.execute("CREATE OR REPLACE TABLE memory_state AS SELECT * FROM memory_state_df")
+# con.execute("CREATE OR REPLACE TABLE memory_state AS SELECT * FROM memory_state_df")
 con.execute("CREATE TABLE IF NOT EXISTS memory_state AS SELECT * FROM memory_state_df")
 
 # ------------------------------------------------------------

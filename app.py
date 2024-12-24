3234a10a585764fda8d6248b819721e48f4b0ac7
Jehan DAVID <jehan.david@pm.me>
2024-12-24 19:03:03 +0100

    check_users_solution function update

--- app.py
+++ app.py
@@ -36,6 +36,9 @@ def check_users_solution(user_query: str) -> None:
     try:
         result = result[solution_df.columns]
         st.dataframe(result.compare(solution_df))
+        if result.compare(solution_df).shape == (0, 0):
+            st.write("Correct !")
+            st.balloons()
     except KeyError as e:
         st.write("Some columns are missing")
     n_lines_difference = result.shape[0] - solution_df.shape[0]

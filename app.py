6d108d04f4ccfced9e243ebd8612dc3a2717a11a
Jehan DAVID <jehan.david@pm.me>
2024-12-24 10:41:20 +0100

    formatting app.py and init_db.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import streamlit as st
 
 con = duckdb.connect(database="data/exercises_sql_tables.duckdb", read_only=False)
 
-with (st.sidebar):
+with st.sidebar:
     theme = st.selectbox(
         "What would you like to review?",
         ("cross_joins", "GroupBy", "Windows Functions"),
@@ -14,8 +14,12 @@ with (st.sidebar):
     )
     st.write("You selected:", theme)
 
-    exercise = con.execute(f"SELECT * FROM memory_state WHERE theme = '{theme}'"
-                           ).df().sort_values("last_reviewed").reset_index()
+    exercise = (
+        con.execute(f"SELECT * FROM memory_state WHERE theme = '{theme}'")
+        .df()
+        .sort_values("last_reviewed")
+        .reset_index()
+    )
     st.write(exercise)
 
     exercise_name = exercise.loc[0, "exercise_name"]
--- init_db.py
+++ init_db.py
@@ -13,12 +13,12 @@ data = {
     "theme": ["cross_joins", "cross_joins"],
     "exercise_name": ["beverages_and_food", "sizes_and_trademarks"],
     "tables": [["beverages", "food_items"], ["sizes", "trademarks"]],
-    "last_reviewed": ["1980-01-01", "1970-01-01"]
+    "last_reviewed": ["1980-01-01", "1970-01-01"],
 }
 memory_state_df = pd.DataFrame(data)
 con.execute("DROP TABLE memory_state")
 con.execute("CREATE TABLE memory_state AS SELECT * FROM memory_state_df")
-#con.execute("CREATE TABLE IF NOT EXISTS memory_state AS SELECT * FROM memory_state_df")
+# con.execute("CREATE TABLE IF NOT EXISTS memory_state AS SELECT * FROM memory_state_df")
 
 # ------------------------------------------------------------
 # CROSS JOIN EXERCISES
@@ -41,22 +41,22 @@ muffin,3
 food_items = pd.read_csv(io.StringIO(csv2))
 con.execute("CREATE TABLE IF NOT EXISTS food_items AS SELECT * FROM food_items")
 
-sizes = '''
+sizes = """
 sizes
 XS
 M
 L
 XL
-'''
+"""
 sizes = pd.read_csv(io.StringIO(sizes))
 con.execute("CREATE TABLE IF NOT EXISTS sizes AS SELECT * FROM sizes")
 
-trademarks = '''
+trademarks = """
 trademarks
 Nike
 Asphalte
 Abercrombie
 Lewis
-'''
+"""
 trademarks = pd.read_csv(io.StringIO(trademarks))
 con.execute("CREATE TABLE IF NOT EXISTS trademarks AS SELECT * FROM trademarks")

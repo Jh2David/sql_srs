cac28aca4cd4a7fc6e8e62c85db9a18be5237641
Jehan DAVID <jehan.david@pm.me>
2024-12-24 10:21:45 +0100

    add exercice cross join sizes and trademarks and sort by last reviewed

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
 # pylint: disable=missing-module-docstring
-import ast
 
 import duckdb
 import streamlit as st
 
 con = duckdb.connect(database="data/exercises_sql_tables.duckdb", read_only=False)
 
-with st.sidebar:
+with (st.sidebar):
     theme = st.selectbox(
         "What would you like to review?",
         ("cross_joins", "GroupBy", "Windows Functions"),
@@ -15,7 +14,8 @@ with st.sidebar:
     )
     st.write("You selected:", theme)
 
-    exercise = con.execute(f"SELECT * FROM memory_state WHERE theme = '{theme}'").df()
+    exercise = con.execute(f"SELECT * FROM memory_state WHERE theme = '{theme}'"
+                           ).df().sort_values("last_reviewed").reset_index()
     st.write(exercise)
 
     exercise_name = exercise.loc[0, "exercise_name"]
@@ -25,7 +25,7 @@ with st.sidebar:
     solution_df = con.execute(answer).df()
 
 st.header("enter your code:")
-query = st.text_area(label="votre code SQL ici", key="user_input")
+query = st.text_area(label="Votre code SQL ici", key="user_input")
 if query:
     result = con.execute(query).df()
     st.dataframe(result)
@@ -45,8 +45,7 @@ if query:
 tab2, tab3 = st.tabs(["Tables", "Solution"])
 
 with tab2:
-    # Transform string into a list even it looks a like at the first view
-    exercise_tables = ast.literal_eval(exercise.loc[0, "tables"])
+    exercise_tables = exercise.loc[0, "tables"]
     for table in exercise_tables:
         st.write(f"table: {table}")
         df_table = con.execute(f"SELECT * FROM {table}").df()

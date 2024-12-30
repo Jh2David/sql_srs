6c5d0694f1efa1378d5131c35805a13d02813020
Jehan DAVID <jehan.david@pm.me>
2024-12-30 10:20:51 +0100

    add init_full_outer_joins functions in init_db.py

--- init_db.py
+++ init_db.py
@@ -3,6 +3,7 @@ import duckdb
 from functions.init_cross_joins import init_cross_joins
 from functions.init_inner_joins import init_inner_joins
 from functions.init_left_joins import init_left_joins
+from functions.init_full_outer_joins import init_full_outer_joins
 
 # DuckDB connection
 con = duckdb.connect(database="data/exercises_sql_tables.duckdb", read_only=False)
@@ -24,5 +25,6 @@ con.execute(
 init_cross_joins(con)
 init_inner_joins(con)
 init_left_joins(con)
+init_full_outer_joins(con)
 
 con.close()

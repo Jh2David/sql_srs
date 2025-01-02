d946f00b896eff0d24e21d8b5476483a52377eb6
Jehan DAVID <jehan.david@pm.me>
2025-01-02 12:03:50 +0100

    refacto des questions init_cross_joins

--- functions/init_cross_joins.py
+++ functions/init_cross_joins.py
@@ -14,22 +14,6 @@ def init_cross_joins(con):
     :return: None
     """
 
-    # Table des questions
-    exercises_and_questions = [
-        {
-            "exercise_name": "beverages_and_food",
-            "question": "Créez une table contenant toutes les combinaisons possibles de boissons et d'aliments, avec leurs prix respectifs.",
-        },
-        {
-            "exercise_name": "sizes_and_trademarks",
-            "question": "Créez une table contenant toutes les combinaisons possibles de tailles et de marques.",
-        },
-        {
-            "exercise_name": "sizes_and_trademarks",
-            "question": "Créez une table représentant toutes les combinaisons possibles d'heures et de quarts d'heure.",
-        },
-    ]
-
     # ----------------------------------------------------------------------------------
     # EXERCICE 01
     # ----------------------------------------------------------------------------------
@@ -120,46 +104,52 @@ def init_cross_joins(con):
     """
     )
 
-    # Insérer les exercices et les questions
-    for exercise in exercises_and_questions:
-        exercise_name = exercise["exercise_name"]
-        question = exercise["question"]
-
-        # QUESTIONS
-        con.execute(
-            """
-                    INSERT INTO exercise_questions (theme, exercise_name, question)
-                    VALUES (?, ?, ?)
-                """,
-            ("01_cross_joins", exercise_name, question),
-        )
-
     # ----------------------------------------------------------------------------------
-    # Ajout des entrées dans memory_state
+    # Table + questions pour chaque exercice
     # ----------------------------------------------------------------------------------
-    exercises_and_tables = [
+    exercises = [
         {
             "exercise_name": "beverages_and_food",
             "tables": ["beverages", "food_items"],
+            "question": "Créez une table contenant toutes les combinaisons possibles de boissons et d'aliments, avec leurs prix respectifs.",
         },
         {
             "exercise_name": "sizes_and_trademarks",
             "tables": ["sizes", "trademarks"],
+            "question": "Créez une table contenant toutes les combinaisons possibles de tailles et de marques.",
         },
         {
             "exercise_name": "hours_and_quarters",
             "tables": ["hours", "quarters"],
+            "question": "Créez une table représentant toutes les combinaisons possibles d'heures et de quarts d'heure.",
         },
     ]
 
-    for exercise in exercises_and_tables:
+    # Insérer les exercices et les questions dans les tables `memory_state` et `exercise_questions`
+    for exercise in exercises:
         exercise_name = exercise["exercise_name"]
-        tables = json.dumps(exercise["tables"])  # Convertir la liste en chaîne JSON
+        tables = json.dumps(exercise["tables"])  # Convertir en JSON
+        question = exercise["question"]
 
+        # Ajouter dans `memory_state`
+        con.execute(
+            f"""
+                 INSERT INTO memory_state (theme, exercise_name, tables, last_reviewed)
+                 VALUES (
+                     '01_cross_joins',
+                     '{exercise_name}',
+                     '{tables}',
+                     '1970-01-01'
+                 )
+               """
+        )
+
+        # Ajouter dans `exercise_questions`
         con.execute(
             """
-            INSERT INTO memory_state (theme, exercise_name, tables, last_reviewed)
-            VALUES (?, ?, ?, ?)
+            INSERT INTO exercise_questions (theme, exercise_name, question)
+            VALUES (?, ?, ?)
+            ON CONFLICT (theme, exercise_name) DO NOTHING;
             """,
-            ("01_cross_joins", exercise_name, tables, "1970-01-01"),
+            ("01_cross_joins", exercise_name, question),
         )

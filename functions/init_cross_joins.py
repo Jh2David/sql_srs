7a1191f8bca600e947a73674b9aebd48ed31b850
Jehan DAVID <jehan.david@pm.me>
2025-01-02 09:47:13 +0100

    add questions for 01_cross_joins

--- functions/init_cross_joins.py
+++ functions/init_cross_joins.py
@@ -1,9 +1,44 @@
 import io
+import json
 
 import pandas as pd
 
 
 def init_cross_joins(con):
+    """
+    Initializes the database for the "01_cross_joins" theme by creating tables and
+    populating them with data. It also sets up several exercises and their questions
+    related to cross joins.
+
+    :param con: DuckDB connection object to execute SQL queries.
+    :return: None
+    """
+
+    # Table des questions
+    con.execute(
+        """
+            CREATE TABLE IF NOT EXISTS exercise_questions (
+                theme TEXT,
+                exercise_name TEXT,
+                question TEXT
+            )
+        """
+    )
+    exercises_and_questions = [
+        {
+            "exercise_name": "beverages_and_food",
+            "question": "Créez une table contenant toutes les combinaisons possibles de boissons et d'aliments, avec leurs prix respectifs.",
+        },
+        {
+            "exercise_name": "sizes_and_trademarks",
+            "question": "Créez une table contenant toutes les combinaisons possibles de tailles et de marques.",
+        },
+        {
+            "exercise_name": "sizes_and_trademarks",
+            "question": "Créez une table représentant toutes les combinaisons possibles d'heures et de quarts d'heure.",
+        },
+    ]
+
     # ----------------------------------------------------------------------------------
     # EXERCICE 01
     # ----------------------------------------------------------------------------------
@@ -93,3 +128,47 @@ def init_cross_joins(con):
         VALUES ('01_cross_joins', 'hours_and_quarters', '["hours", "quarters"]', '1970-01-01')
     """
     )
+
+    # Insérer les exercices et les questions
+    for exercise in exercises_and_questions:
+        exercise_name = exercise["exercise_name"]
+        question = exercise["question"]
+
+        # QUESTIONS
+        con.execute(
+            """
+                    INSERT INTO exercise_questions (theme, exercise_name, question)
+                    VALUES (?, ?, ?)
+                """,
+            ("01_cross_joins", exercise_name, question),
+        )
+
+    # ----------------------------------------------------------------------------------
+    # Ajout des entrées dans memory_state
+    # ----------------------------------------------------------------------------------
+    exercises_and_tables = [
+        {
+            "exercise_name": "beverages_and_food",
+            "tables": ["beverages", "food_items"],
+        },
+        {
+            "exercise_name": "sizes_and_trademarks",
+            "tables": ["sizes", "trademarks"],
+        },
+        {
+            "exercise_name": "hours_and_quarters",
+            "tables": ["hours", "quarters"],
+        },
+    ]
+
+    for exercise in exercises_and_tables:
+        exercise_name = exercise["exercise_name"]
+        tables = json.dumps(exercise["tables"])  # Convertir la liste en chaîne JSON
+
+        con.execute(
+            """
+            INSERT INTO memory_state (theme, exercise_name, tables, last_reviewed)
+            VALUES (?, ?, ?, ?)
+            """,
+            ("01_cross_joins", exercise_name, tables, "1970-01-01"),
+        )

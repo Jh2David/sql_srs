6903217d8804289f414c1b503c655c7e855ce917
Jehan DAVID <jehan.david@pm.me>
2025-01-04 17:01:04 +0100

    add exercices for football

---
+++ answers/07_case_when/ex03B_case_when_football.sql
@@ -0,0 +1,15 @@
+SELECT Div,
+    AVG (
+        CASE
+            WHEN data.HomeTeam = 'Lille' AND FTHG > FTAG THEN 1
+            WHEN data.HomeTeam = 'Lille' AND FTHG <= FTAG THEN 0
+        END
+    ) AS lille_wins_home,
+    AVG (
+        CASE
+            WHEN data.AwayTeam = 'Lille' AND FTHG < FTAG THEN 1
+            WHEN data.AwayTeam = 'Lille' AND FTHG >= FTAG THEN 0
+        END
+    ) AS lille_wins_away,
+FROM data
+GROUP BY Div
